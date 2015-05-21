ad9ea82cbb25dc8f157378f3d72bbd0f5a82e464
Joseph Mortensen <joemort@gmail.com>
2015-05-21 17:44:24 -0600

    candidate for final version of movies until next weeks assignment

--- viewMovies.php
+++ viewMovies.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Team Scripture Activity</title>
+    <title>Movie Preferences and Suggestions</title>
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
@@ -15,7 +15,8 @@
 $dbName = 'php';
 $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
 $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
-$userID = 1;
+$userID = 1; // TODO: this will be from a session, but since we don't have
+//database modification yet, we don't have a login/register page.
 
 try
 {
