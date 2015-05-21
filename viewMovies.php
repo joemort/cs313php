5caec663521778a0185384c7a92cb9c900ebb733
Joseph Mortensen <joemort@gmail.com>
2015-05-21 17:13:05 -0600

    another attempt for movies

--- viewMovies.php
+++ viewMovies.php
@@ -6,25 +6,24 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <style>
-.row-same-height {
-  display: table;
-  width: 100%;
-  table-layout: fixed;
-}   
-
-td {
-  padding: 10px;
-}
 
 table {
   height: 650px;
+}
+
+.smallheader {
+  height: 100px;
+}
+
+.imgrow {
+   height: 300px;
 }
     </style>
 </head>
 
 <body>
 <h2>Movies:</h2>
-<div class="row-fluid row-same-height">
+<div class="row-fluid">
 <?php
 $dbName = 'php';
 $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
@@ -63,8 +62,8 @@ on suggestions.suggestion_id=movies.id');#'CALL getMoviesList(' . $userID . ')')
         } else {
             echo 'normal';
         }
-        echo '"><table><tr><td><strong>' . $row['name'] . '</strong></td></tr>'
-            . '<tr><td><img src="movieposters/' . $row['image'] . '" width="200" height="300"/></td></tr>'
+        echo '"><table><tr><td class="smallheader"><strong>' . $row['name'] . '</strong></td></tr>'
+            . '<tr><td class="imgrow"><img src="movieposters/' . $row['image'] . '" width="200" height="300"/></td></tr>'
             . '<tr><td>' . $row['description'] . "</p></td></tr></table></div>\n\n";
 	}
 }
