7b83e7d7a6037c8648f508f18c3d59c25793d4f4
Joseph Mortensen <joemort@gmail.com>
2015-05-30 12:33:47 -0600

    start adding ability to like/dislike a movie

--- viewMovies.php
+++ viewMovies.php
@@ -11,7 +11,7 @@
 <head>
     <title>Movie Preferences and Suggestions</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-2.0.2.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="viewMovies.css"/>
     <script type="text/javascript">
