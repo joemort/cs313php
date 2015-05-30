8aff03029def129abba0c8991f79ac87c2171a38
Joseph Mortensen <joemort@gmail.com>
2015-05-30 11:37:30 -0600

    start basics for registering users

--- viewMovies.php
+++ viewMovies.php
@@ -10,9 +10,9 @@
 <html>
 <head>
     <title>Movie Preferences and Suggestions</title>
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="viewMovies.css"/>
 </head>
 
