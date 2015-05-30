a3e424fa843f00e04cfe2e21818cb8f0b6c9af5e
Joseph Mortensen <joemort@gmail.com>
2015-05-30 09:50:18 -0600

    start basics for registering users

--- doLogin.php
+++ doLogin.php
@@ -1,5 +1,67 @@
 <?php
+
     require 'password.php';
+    
+    function loginExisting() {
+        try {
+            $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", "test", "test");
+            //TODO: change this to a sproc.
+            $statement = $db->prepare("select id from users where name=:name and password=:password");
+            $statement->bindParam(':name', $username);
+            $statement->bindParam(':password', $passwordHash);
+            $statement->execute();
+            $row = $statement->fetch(PDO::FETCH_ASSOC);
+            if (isset($row) && isset($row['id']))
+            {
+                $_SESSION['userid'] = $row['id'];
+                header( 'Location: viewMovies.php');
+            }
+            else
+            {
+                $_SESSION['failure'] = "Username/Password was not correct. Please try again.";
+                header( 'Location: loginPage.php');
+            }
+        
+            $statement->close();
+            $db->close();
+        }
+        catch (Exception $ex)
+        {
+            echo "Can't connect to DB. Exception: $ex";
+            die();
+        }
+    }
+    
+    function registerNew() {
+        try {
+            $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", "test", "test");
+            //TODO: change this to a sproc.
+            $statement = $db->prepare("insert into users (name, password) values (:name, :password);");
+            $statement->bindParam(':name', $username);
+            $statement->bindParam(':password', $passwordHash);
+            $statement->execute();
+            $insertedId = $db->lastInsertId();
+            
+            if ($insertedId > 0)
+            {
+                $_SESSION['failure'] = "Registration successful. Please log in.";
+                header( 'Location: loginPage.php');
+            }
+            else
+            {
+                $_SESSION['failure'] = "Username exists, please try a different one.";
+                header( 'Location: loginPage.php');
+            }
+        
+            $statement->close();
+            $db->close();
+        }
+        catch (Exception $ex)
+        {
+            echo "Can't connect to DB. Exception: $ex";
+            die();
+        }
+    }
     
     session_start();
     if(!isset($_POST['user']) || !isset($_POST['password'])) {
@@ -21,37 +83,8 @@
         $_SESSION['failure'] = "used login button.";
         header( 'Location: loginPage.php');
     } else if (isset($_POST['register'])) {
-        $_SESSION['failure'] = "used register button.";
-        header( 'Location: loginPage.php');
+        registerNew();
     } else {
-        $_SESSION['failure'] = "couldn't detect button :(.";
-        header( 'Location: loginPage.php');
-    }/*
-    try {
-        $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", "test", "test");
-        $statement = $db->prepare("select id from users where name=:name and password=:password");
-        $statement->bindParam(':name', $username);
-        $statement->bindParam(':password', $passwordHash);
-        $statement->execute();
-        $row = $statement->fetch(PDO::FETCH_ASSOC);
-        if (isset($row) && isset($row['id']))
-        {
-            $_SESSION['userid'] = $row['id'];
-            $_SESSION['hash'] = $passwordHash;
-            header( 'Location: viewMovies.php');
-        }
-        else
-        {
-            $_SESSION['failure'] = "Username/Password was not correct. Please try again.";
-            header( 'Location: loginPage.php');
-        }
-        
-        $statement->close();
-        $db->close();
+        loginExisting();
     }
-    catch (Exception $ex)
-    {
-        echo "Can't connect to DB. Exception: $ex";
-        die();
-    }*/
 ?>
